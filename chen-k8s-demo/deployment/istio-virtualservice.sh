c90c61b853edd9b9fa128090b341aceb7273cb38
Eric Chen <eric.chen@f5.com>
2019-07-16 00:36:55 +0000

    additional resources for "sorting k8s demo"

---
+++ chen-k8s-demo/deployment/istio-virtualservice.sh
@@ -0,0 +1,23 @@
+kubectl apply -n istio-demo -f - <<EOF
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: httpbin
+spec:
+  hosts:
+  - "httpbin.example.com"
+  gateways:
+  - httpbin-gateway
+  http:
+  - match:
+    - uri:
+        prefix: /status
+    - uri:
+        prefix: /delay
+    route:
+    - destination:
+        port:
+          number: 8000
+        host: httpbin
+EOF
+
