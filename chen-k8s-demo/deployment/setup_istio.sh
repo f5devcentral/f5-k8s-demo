a693d91c838be585a42ea462f2ad7df31b1e1c5f
Eric Chen <eric.chen@f5.com>
2019-07-16 20:01:34 +0000

    add virtualservice / gateway

--- chen-k8s-demo/deployment/setup_istio.sh
+++ chen-k8s-demo/deployment/setup_istio.sh
@@ -7,3 +7,47 @@ kubectl apply -f echo-deployment.yaml
 kubectl apply -f my-echo-service.yaml
 kubectl apply -f httpbin.yaml -n istio-demo
 kubectl apply -f istio-service.yaml -n istio-system
+kubectl -n istio-demo apply -f - <<EOF
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: httpbin-gateway
+spec:
+  selector:
+    istio: ingressgateway # use istio default ingress gateway
+  servers:
+  - port:
+      number: 443
+      name: https
+      protocol: HTTPS
+    tls:
+      mode: MUTUAL
+      serverCertificate: /etc/istio/ingressgateway-certs/tls.crt
+      privateKey: /etc/istio/ingressgateway-certs/tls.key
+      caCertificates: /etc/istio/ingressgateway-ca-certs/ca-chain.cert.pem
+    hosts:
+    - "httpbin.example.com"
+EOF
+kubectl apply -n istio-demo -f - <<EOF
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: httpbin
+spec:
+  hosts:
+  - "httpbin.example.com"
+  gateways:
+  - httpbin-gateway
+  http:
+  - match:
+    - uri:
+        prefix: /status
+    - uri:
+        prefix: /delay
+    route:
+    - destination:
+        port:
+          number: 8000
+        host: httpbin
+EOF
+
