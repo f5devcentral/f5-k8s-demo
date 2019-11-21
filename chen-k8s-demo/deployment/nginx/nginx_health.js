5cd71ff7d60d23c76fc76c92bc8a4ab5a32e92a6
Eric Chen <eric.chen@f5.com>
2019-11-21 20:27:22 +0000

    custom nginx health probe

---
+++ chen-k8s-demo/deployment/nginx/nginx_health.js
@@ -0,0 +1,38 @@
+function status_by_fqdn(r) {
+    r.subrequest('/api/5/http/upstreams', {
+            method: 'GET'
+        },
+        function(res) {
+            var output = {};
+            var myRe = /[a-zA-Z0-9]+\.f5demo\.com/;
+            var needle = r.args["fqdn"];
+                function filterUp(item) {
+                    if (item.state === "up") {
+                        return true;
+                    }
+                    return false;
+                }
+
+            if (res.status == 200) {
+                var input = JSON.parse(res.responseBody);
+                for (var u in input) {
+                  var tmp = myRe.exec(u);
+                    if( tmp && tmp[0] === needle ) {
+                        var cnt = input[u]['peers'].filter(filterUp).length;
+                        if(cnt) {
+                            r.return(res.status, JSON.stringify({"status":true}));
+                            return;
+                        } else {
+                            r.return(res.status, JSON.stringify({"status":false}));
+                            return;
+                        }
+
+                 }
+              }
+                r.return(res.status, JSON.stringify({"status":false}));                
+                return;
+            }
+            r.return(500);
+        });
+
+}
