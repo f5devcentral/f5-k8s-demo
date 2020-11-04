c5ada0ad26a71b6aaee4b4d04dcb1b80c5cfb930
Eric Chen <eric.chen@f5.com>
2020-11-04 07:10:04 -0500

    updated CRD example

--- mytypelb/README.md
+++ mytypelb/README.md
@@ -1,4 +1,86 @@
-# MyTypeLB
+# MyTypeLB: CRD
+
+The following is a simple PoC of using type:LoadBalancer with
+Container Ingress Services 2.2.0 it generates TransportServer
+CRD instances to support L4 TCP services.
+
+This requires that you are running CIS in CRD mode!
+
+The process is to assign a service that includes "loadBalancerIP"
+
+This script will identify these resources and create a BIG-IP
+VirtaulServer with either the  IP address specified or pick from
+an allocated range of IPs.
+
+## Installing the script
+
+```
+pip install -r requirements.txt
+```
+## Setting up the config
+
+Modify 'data.json' to match your network and create a configuration file.
+The script is hardcoded to look for a configmap "chenpam" in the namespace
+"default"
+
+```
+kubectl create cm chenpam --from-file=config=data.json
+```
+
+## Running the Script
+
+This script assumes that you have a kubeconfig file with
+appropriate privileges.
+
+```
+python ./crdtypelb.py
+```
+
+## expected output.
+
+If you start with two services w/out an IP.
+
+```
+$ oc get svc my-crd
+NAME     TYPE           CLUSTER-IP      EXTERNAL-IP   PORT(S)                      AGE
+my-crd   LoadBalancer   172.30.83.160   <pending>     80:30213/TCP,443:31861/TCP   21s
+$ oc get svc -n nginx-ingress nginx-ingress-crd
+NAME                TYPE           CLUSTER-IP       EXTERNAL-IP   PORT(S)                      AGE
+nginx-ingress-crd   LoadBalancer   172.30.107.195   <pending>     80:30150/TCP,443:30485/TCP   42s
+```
+After you run the script you should see the status changed.
+```$ oc get svc my-crd
+NAME     TYPE           CLUSTER-IP      EXTERNAL-IP   PORT(S)                      AGE
+my-crd   LoadBalancer   172.30.83.160   10.1.10.80    80:30213/TCP,443:31861/TCP   103s
+$ oc get svc -n nginx-ingress nginx-ingress-crd
+NAME                TYPE           CLUSTER-IP       EXTERNAL-IP   PORT(S)                      AGE
+nginx-ingress-crd   LoadBalancer   172.30.107.195   10.1.10.81    80:30150/TCP,443:30485/TCP   103s
+```
+You will also see the script has created "TransportServer" entries.
+```
+$ oc get transportservers --all-namespaces
+NAMESPACE       NAME                    AGE
+default         my-crd-443              75m
+default         my-crd-80               75m
+nginx-ingress   nginx-ingress-crd-443   75m
+nginx-ingress   nginx-ingress-crd-80    75m
+```
+These entries are used by CIS to generate the BIG-IP configuration.
+```
+admin@(bigip1)(cfg-sync Standalone)(Active)(/ocp)(tmos)# list /ltm virtual recursive one-line
+ltm virtual Shared/crd_10_1_10_80_80 { creation-time 2020-11-04:04:06:38 description Shared destination 10.1.10.80:80 ip-protocol tcp last-modified-time 2020-11-04:04:06:38 mask 255.255.255.255 partition ocp persist { /Common/source_addr { default yes } } pool Shared/default_my_crd_8080 profiles { /Common/f5-tcp-progressive { } } serverssl-use-sni disabled source 0.0.0.0/0 source-address-translation { type automap } translate-address enabled translate-port enabled vs-index 48 }
+ltm virtual Shared/crd_10_1_10_80_443 { creation-time 2020-11-04:04:06:38 description Shared destination 10.1.10.80:443 ip-protocol tcp last-modified-time 2020-11-04:04:06:38 mask 255.255.255.255 partition ocp persist { /Common/source_addr { default yes } } pool Shared/default_my_crd_8443 profiles { /Common/f5-tcp-progressive { } } serverssl-use-sni disabled source 0.0.0.0/0 source-address-translation { type automap } translate-address enabled translate-port enabled vs-index 47 }
+ltm virtual Shared/crd_10_1_10_81_80 { creation-time 2020-11-04:04:06:38 description Shared destination 10.1.10.81:80 ip-protocol tcp last-modified-time 2020-11-04:04:06:38 mask 255.255.255.255 partition ocp persist { /Common/source_addr { default yes } } pool Shared/nginx_ingress_nginx_ingress_crd_443 profiles { /Common/f5-tcp-progressive { } } serverssl-use-sni disabled source 0.0.0.0/0 source-address-translation { type automap } translate-address enabled translate-port enabled vs-index 50 }
+ltm virtual Shared/crd_10_1_10_81_443 { creation-time 2020-11-04:04:06:38 description Shared destination 10.1.10.81:443 ip-protocol tcp last-modified-time 2020-11-04:04:06:38 mask 255.255.255.255 partition ocp persist { /Common/source_addr { default yes } } pool Shared/nginx_ingress_nginx_ingress_crd_443 profiles { /Common/f5-tcp-progressive { } } serverssl-use-sni disabled source 0.0.0.0/0 source-address-translation { type automap } translate-address enabled translate-port enabled vs-index 49 }
+```
+
+
+## Known issues
+
+-hardcodes "default" namespace and name of configmap
+-does not handle any DNS 
+
+# MyTypeLB: ConfigMap
 
 The following is a simple PoC of using type:LoadBalancer with
 Container Ingress Services 2.2.0
@@ -22,11 +104,9 @@ appropriate privileges.
 ```
 # using AS3 configmap
 ./mytypelb.py
-# using CRD TransportServer
-./crdtypelb.py
 ```
 
 ## Known issues
 
--Only uses a single port from the service (workaround: create multiple services with same loadBalancerIP <-- for ConfigMap
+-Only uses a single port from the service (workaround: create multiple services with same loadBalancerIP 
 -hardcodes "default" namespace and name of configmap
--- mytypelb/crdtypelb.py
+++ mytypelb/crdtypelb.py
@@ -29,6 +29,7 @@
 from kubernetes import client, config
 import json
 import sys
+from ipaddress import ip_network
 # Configs can be set in Configuration class directly or using helper utility
 config.load_kube_config()
 
@@ -36,36 +37,73 @@ v1 = client.CoreV1Api()
 api = client.CustomObjectsApi()
 #print("Listing pods with their IPs:")
 #ret = v1.list_pod_for_all_namespaces(watch=False)
-ret = v1.list_service_for_all_namespaces(watch=False)
+
 crdmap = {}
 # hardcoded values
 namespace = "default"
+
+# base config
+#
+# This is a very simple local IPAM known as "chenpam"
+#
+# It relies on a ConfigMap that contains a list of IP ranges
+# the LoadBalancerIP will be picked either from one provided by
+# the end-user or from the list
+#
+base_configmap = 'default/chenpam'
+(base_configmap_namespace,base_configmap_name) = base_configmap.split('/')
+ret = v1.read_namespaced_config_map(base_configmap_name, base_configmap_namespace)
+
+base_config = {'ranges':['10.1.10.0/24'],
+               'allocated':[],
+               'conflict':[],}
+base_config = json.loads(ret.data['config'])
+my_configmap = ret
+
+all_ips = []
+for range in base_config['ranges']:
+    all_ips.extend([str(a) for a in ip_network(range).hosts()])
+
+next_idx = all_ips.index(base_config['next'])
+
+# grab all services
+
+ret = v1.list_service_for_all_namespaces(watch=False)
 
 for i in ret.items:
     # only look for type LoadBalancer
     if i.spec.type != "LoadBalancer":
         continue
-
+    print(i.status.load_balancer.ingress)
+    if i.status.load_balancer.ingress:
+        ip_addr = i.status.load_balancer.ingress[0].ip
     # only inspect services that specify a load balancer IP
-    if i.spec.load_balancer_ip:
-        # add status
-        i.status.load_balancer.ingress = [{'ip':i.spec.load_balancer_ip}]
-        # add labels that will be used by AS3
+    elif i.spec.load_balancer_ip:
+        ip_addr = i.spec.load_balancer_ip
+    else:
+        ip_addr = all_ips[next_idx]
+        next_idx+=1
+        base_config['allocated'].append(ip_addr)
+    # add to list
 
-        servicePorts = [(p.port,p.target_port) for p in  i.spec.ports]
-        crdmap["%s_%s" %(i.metadata.namespace,i.metadata.name)] = (i.spec.load_balancer_ip, servicePorts, i.metadata.name, i.metadata.namespace)
-        # update service with labels and status
-        v1.replace_namespaced_service_status(i.metadata.name,i.metadata.namespace,i)
+    # add status
+    i.status.load_balancer.ingress = [{'ip':ip_addr,'hostname':"%s.%s.dc1.example.com" %(i.metadata.name,i.metadata.namespace)}]
+    # add labels that will be used by AS3
+
+    servicePorts = [(p.port,p.target_port) for p in  i.spec.ports]
+    crdmap["%s_%s" %(i.metadata.namespace,i.metadata.name)] = (ip_addr, servicePorts, i.metadata.name, i.metadata.namespace)
+    # update service with labels and status
+    v1.replace_namespaced_service_status(i.metadata.name,i.metadata.namespace,i)
+
 
 crd_output = {}
-
 for app, dest in crdmap.items():
     # AS3 definition of TCP service
 
     for port in dest[1]:
         d2 = {'apiVersion': 'cis.f5.com/v1',
          'kind': 'TransportServer',
-         'metadata': {'labels': {'f5cr': 'true'},
+         'metadata': {'labels': {'f5cr': 'true','chenpam':'true'},
                       'name': "%s-%d" %(dest[2],port[0]),
                       'namespace': dest[3]
          },
@@ -79,37 +117,33 @@ for app, dest in crdmap.items():
 
         crd_output["%s_%s-%s" %(dest[3],dest[2],port[0])] = d2
 
-    continue
-    d2 = {app:{'class':'Service_TCP',
-          'virtualAddresses':[dest[0]],
-          'virtualPort':dest[1],
-          'pool':"%s_pool" %(app)},
-     "%s_pool" %(app): {'class':'Pool',
-                        'monitors': [ 'tcp' ],
-                        'members': [{'servicePort': 80, 'serverAddresses': []}]
-                        }
-    }
-
-    d['declaration']['MyTypeLB']['Services'][app] = d2[app]
-    d['declaration']['MyTypeLB']['Services']["%s_pool" %(app)] = d2["%s_pool" %(app)]
 group = 'cis.f5.com'
 version = 'v1'
-ret = api.list_namespaced_custom_object(group, version, namespace, 'transportservers', watch=False)
+#ret = api.list_namespaced_custom_object(group, version, namespace, 'transportservers', watch=False)
+ret = api.list_cluster_custom_object(group, version, 'transportservers', watch=False)
 
 existing_crds = {}
 for i in ret['items']:
 
     crd_name = "%s_%s" %(i['metadata']['namespace'],i['metadata']['name'])
-
     existing_crds[crd_name] = i
 for k,v in crd_output.items():
-
+    crd_namespace = v['metadata']['namespace']
     if k in existing_crds:
         existing_v = existing_crds[k]
-
+        if 'labels' not in existing_v['metadata'] or 'chenpam' not in existing_v['metadata']['labels'] or existing_v['metadata']['labels']['chenpam'] != 'true':
+            continue
         for i in v['spec']:
             existing_v['spec'][i] = v['spec'][i]
-
-        api.replace_namespaced_custom_object(group, version, namespace, 'transportservers', crd_output[k]['metadata']['name'], existing_v)
+        print('updating',k)
+        api.replace_namespaced_custom_object(group, version, crd_namespace, 'transportservers', crd_output[k]['metadata']['name'], existing_v)
     else:
-        api.create_namespaced_custom_object(group, version, namespace, 'transportservers', crd_output[k])
+        print('creating',k,crd_output[k])
+        api.create_namespaced_custom_object(group, version, crd_namespace, 'transportservers', crd_output[k])
+
+# update config
+new_next = all_ips[next_idx]
+print(new_next)
+base_config['next'] = new_next
+my_configmap.data['config'] = json.dumps(base_config)
+v1.replace_namespaced_config_map(base_configmap_name, base_configmap_namespace, my_configmap)
